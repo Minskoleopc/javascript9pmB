940bdf7fa5630167058349fb6dafc5ba47c5c836
Minskoleopc <minskoleopc@gmail.com>
2022-11-03 22:07:37 +0530

    added

---
+++ cypress/integration/3-login/login.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+// Arrange
+// open the url 
+
+
+// Action 
+// Enter the username 
+// Enter the password
+// Click on login button
+
+// Assertion
+// Verify dashboard  url
