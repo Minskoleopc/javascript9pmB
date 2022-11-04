1c9a8acad54d462594abddf5173e9bcc6aa0dfc8
Minskoleopc <minskoleopc@gmail.com>
2022-11-04 21:53:47 +0530

    day2

--- cypress/integration/3-login/login.spec.js
+++ cypress/integration/3-login/login.spec.js
@@ -1,13 +1,57 @@
 /// <reference types="cypress" />
 
-// Arrange
-// open the url 
+// test scenario ----- what to test ?
+// test case   ------ how to test?
+
+describe('login functionality', function () {
+
+    it('verify with valid credentials', function () {
+        // Arrange
+        // open the url 
+        cy.visit('https://www.saucedemo.com/')
+        // Action 
+        // Enter the username 
+        cy.get('#user-name').type("standard_user")
+        // Enter the password
+        cy.get('#password').type("secret_sauce")
+        // Click on login button
+        cy.get('#login-button').click()
+        // Assertion
+        // Verify dashboard  url
+        cy.get('.title').should('be.visible')
+
+    })
+    it('verify with invalid credentials', function () {
+
+               // Arrange
+        // open the url 
+        cy.visit('https://www.saucedemo.com/')
+        // Action 
+        // Enter the username 
+        cy.get('#user-name').type("standard_us")
+        // Enter the password
+        cy.get('#password').type("secret_sau")
+        // Click on login button
+        cy.get('#login-button').click()
+        // Assertion
+        // Verify dashboard  url
+        cy.get('.error-button').should('be.visible')
 
 
-// Action 
-// Enter the username 
-// Enter the password
-// Click on login button
 
-// Assertion
-// Verify dashboard  url
+    })
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
